67c4b180849c8b1e66bb987e5e5fbf0771d91af9
Damien Erambert <damien@erambert.me>
2024-06-02 11:21:43 -0700

    minify html/css literals

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import minifyHTML from "rollup-plugin-minify-html-literals";
+
 /** @type {import('vite').UserConfig} */
 export default {
   build: {
     assetsInlineLimit: 0,
   },
+  plugins: [minifyHTML.default()],
 };
