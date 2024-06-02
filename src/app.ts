16ef31c94cda9b8606ed0431dacc0dda26f4f013
Damien Erambert <damien@erambert.me>
2024-06-02 01:06:32 -0700

    default avatar

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { LitElement, css, html } from "lit";
 import { customElement, state } from "lit/decorators.js";
 import { UploadValidFileEvent } from "./events";
 import { AvatarMask } from "./components/avatar-preview";
+import defaultAvatar from "./assets/default-avatar.jpg";
 
 enum Warnings {
   TOO_BIG = "TOO_BIG",
@@ -17,8 +18,7 @@ export class App extends LitElement {
   }
 
   @state()
-  previewAvatarSrc =
-    "https://staging.cohostcdn.org/avatar/291-71d40cfe-58e3-4a64-84d5-054fbc0e2edd-profile.jpg?dpr=2&width=80&height=80&fit=cover&auto=webp";
+  previewAvatarSrc = defaultAvatar;
 
   @state()
   avatarMask: AvatarMask = "circle";
