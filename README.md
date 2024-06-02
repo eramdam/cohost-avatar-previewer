a1131b6b1cb6a006b90e3ed0a683b8a7fc130d45
Damien Erambert <damien@erambert.me>
2024-06-02 00:50:57 -0700

    :memo:

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cohost-avatar-previewer
+# Cohost Avatar Previewer
+
+Like it says on the tin, a simple web page to quickly see how your avatar looks like in most of [cohost](https://cohost.org)'s UI.
+
+Also an excuse for me to learn [Lit](https://lit.dev/)
--- src/components/upload-input.ts
+++ src/components/upload-input.ts
@@ -1,5 +1,5 @@
 import { LitElement, css, html } from "lit";
-import { customElement, query, state } from "lit/decorators.js";
+import { customElement, query } from "lit/decorators.js";
 import { UploadValidFileEvent } from "../events";
 
 @customElement("upload-input")
